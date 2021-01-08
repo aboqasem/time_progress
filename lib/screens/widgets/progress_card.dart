097dafd0be35b15a80c74aa47b58d6c5cc0778a7
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-08 15:07:10 +0800

    Add basic ProgressCard

---
+++ lib/screens/widgets/progress_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+class ProgressCard extends StatelessWidget {
+  final String title;
+  final double progress;
+
+  ProgressCard({
+    @required this.title,
+    @required this.progress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(2.0.h),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(title, style: TextStyle(fontFamily: 'YeonSung')),
+                Text(
+                  '${progress.toStringAsFixed(8)}%',
+                  style: TextStyle(fontFamily: 'YeonSung'),
+                ),
+              ],
+            ),
+            SizedBox(height: 1.0.h),
+            LinearProgressIndicator(value: progress / 100.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
