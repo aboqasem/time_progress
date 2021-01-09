50c4e625bddce9d29ef3089692e08f4b59746759
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-09 08:47:26 +0800

    Implement Banner Ad

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:time_progress/screens/widgets/progress_cards_list_view.dart';
+import 'package:time_progress/services/ad_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    AdService.loadBannerAd();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,5 +24,11 @@ class HomeScreen extends StatelessWidget {
         child: ProgressCardListView(),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    AdService.dispose();
+    super.dispose();
   }
 }
