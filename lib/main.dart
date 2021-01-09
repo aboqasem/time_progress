f7a5eb594cbd7196964473fbb85ca70b66e44784
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-09 08:46:37 +0800

    Initialize FirebaseAdMob

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'package:time_progress/screens/home_screen.dart';
+import 'package:time_progress/services/ad_service.dart';
 import 'package:time_progress/theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FirebaseAdMob.instance.initialize(appId: AdService.appId);
   runApp(MyApp());
 }
 
