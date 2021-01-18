862b587ed8a3c63e6fcf3faa69890be13415a103
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-18 12:52:04 +0800

    Remove hash from URL

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:time_progress/screens/home_screen.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(MyApp());
 }
 
