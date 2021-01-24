c48903ab6f818801d993b28c58473c247a152ac4
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-24 09:00:41 +0800

    Add progress intervals model

---
+++ lib/models/progress_intervals.dart
@@ -0,0 +1,24 @@
+import 'package:time_progress/models/progress_interval.dart';
+
+class ProgressIntervals {
+  static ProgressIntervals _progressIntervals;
+  final Map<String, ProgressInterval> _intervals = {};
+
+  ProgressIntervals._();
+
+  factory ProgressIntervals() {
+    if (_progressIntervals == null) _progressIntervals = ProgressIntervals._();
+    return _progressIntervals;
+  }
+
+  int get length => _intervals.length;
+
+  ProgressInterval operator [](String key) => _intervals[key];
+
+  void operator []=(String key, ProgressInterval value) =>
+      _intervals[key] = value;
+
+  bool containsKey(String key) => _intervals.containsKey(key);
+
+  void addAll(Map<String, ProgressInterval> other) => _intervals.addAll(other);
+}
