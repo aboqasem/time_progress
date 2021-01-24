553f17c8ca333b75f78849991a02fb7b9773135a
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-24 18:28:24 +0800

    Add storage service

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:time_progress/models/progress_interval.dart';
+
+class StorageService {
+  static final StorageService instance = StorageService._();
+
+  StorageService._();
+
+  Future<bool> storeProgressInterval(String key, ProgressInterval value) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return await preferences.setStringList(
+      key,
+      [value.begin.toString(), value.end.toString()],
+    );
+  }
+
+  Future<Map<String, ProgressInterval>> retrieveProgressIntervals() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    final Set<String> keys = preferences.getKeys();
+
+    final Map<String, ProgressInterval> progressIntervals = {};
+    keys.forEach((String key) {
+      final List<String> dates = preferences.getStringList(key);
+      progressIntervals[key] = ProgressInterval(
+        begin: DateTime.parse(dates[0]),
+        end: DateTime.parse(dates[1]),
+      );
+    });
+    return progressIntervals;
+  }
+}
