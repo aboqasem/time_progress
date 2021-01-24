fbc9a9fb2fad28ef27f78c8ad983b9f6396109bb
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-24 13:03:05 +0800

    Add progress interval additionsheet

---
+++ lib/screens/widgets/add_progress_interval_sheet.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:time_progress/extensions/responsive_length.dart';
+import 'package:time_progress/screens/widgets/progress_interval_form.dart';
+
+class AddProgressIntervalSheet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: 15.0.vw(context),
+          top: 40.0,
+          right: 15.0.vw(context),
+          bottom: 40.0 + MediaQuery.of(context).viewInsets.bottom,
+        ),
+        child: Column(
+          children: [
+            Text(
+              'Add Progress Interval',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: min(5.0.vw(context), 35.0),
+              ),
+            ),
+            ProgressIntervalForm(),
+          ],
+        ),
+      ),
+    );
+  }
+}
