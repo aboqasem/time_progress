f63c0c4589b85a71704c33050e154c1024febe18
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-24 12:53:51 +0800

    Add constants

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:intl/intl.dart';
+
+DateFormat kFormDateFormat = DateFormat('E, dd-MM-y');
+
+DateFormat kFormTimeFormat = DateFormat('hh:mm a');
+
+DateFormat kCardDateTimeFormat = DateFormat('E, dd-MM-y hh:mm a');
