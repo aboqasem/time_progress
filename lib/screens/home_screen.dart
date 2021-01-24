08dac456c71ec35489a9239dd25ae18f34d81275
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-24 13:04:03 +0800

    Implement progress interval addition

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:time_progress/screens/widgets/add_progress_interval_sheet.dart';
 import 'package:time_progress/screens/widgets/progress_cards_list_view.dart';
 import 'package:time_progress/extensions/responsive_length.dart';
 
@@ -14,9 +15,25 @@ class HomeScreen extends StatelessWidget {
           style: TextStyle(fontSize: min(5.0.vw(context), 35.0)),
         ),
       ),
-      body: SafeArea(
-        child: ProgressCardListView(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        width: min(15.0.vw(context), 90.0),
+        height: min(15.0.vw(context), 90.0),
+        child: FloatingActionButton(
+          child: Icon(Icons.add, size: min(5.0.vw(context), 35.0)),
+          onPressed: () async {
+            await showModalBottomSheet(
+              context: context,
+              builder: (_) => AddProgressIntervalSheet(),
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+              ),
+            );
+          },
+        ),
       ),
+      body: SafeArea(child: ProgressCardListView()),
     );
   }
 }
