2efb7de8e69919808274340a9d2a7a383428df71
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-01-24 13:02:33 +0800

    Add progress interval form

---
+++ lib/screens/widgets/progress_interval_form.dart
@@ -0,0 +1,110 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:time_progress/extensions/responsive_length.dart';
+import 'package:time_progress/models/progress_interval.dart';
+import 'package:time_progress/models/progress_intervals.dart';
+import 'package:time_progress/screens/widgets/primary_button.dart';
+import 'package:time_progress/screens/widgets/progress_interval_date_picker.dart';
+
+class ProgressIntervalForm extends StatefulWidget {
+  static final GlobalKey<FormState> _key = GlobalKey<FormState>();
+
+  @override
+  _ProgressIntervalFormState createState() => _ProgressIntervalFormState();
+}
+
+class _ProgressIntervalFormState extends State<ProgressIntervalForm> {
+  final ProgressIntervals _intervals = ProgressIntervals();
+  String title;
+  DateTime beginDate = DateTime.now();
+  DateTime endDate = DateTime.now().add(Duration(minutes: 1));
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: ProgressIntervalForm._key,
+      child: Column(
+        children: [
+          TextFormField(
+            autofocus: true,
+            textAlign: TextAlign.center,
+            cursorColor: Colors.tealAccent,
+            keyboardAppearance: Brightness.dark,
+            style: TextStyle(fontSize: min(4.0.vw(context), 30.0)),
+            onChanged: (String value) => title = value,
+            validator: (String value) {
+              if (value.isEmpty) return 'Please type in a title.';
+              if (_intervals.containsKey(value))
+                return 'This title already exists.';
+              return null;
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter progress interval title',
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.tealAccent),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.tealAccent, width: 3.0),
+              ),
+              errorStyle: TextStyle(fontSize: min(3.5.vw(context), 25.0)),
+            ),
+          ),
+          SizedBox(height: 20.0),
+          ProgressIntervalDatePicker(
+            beginDate: beginDate,
+            endDate: endDate,
+            onBeginDateChange: (DateTime date) => setState(() {
+              beginDate = DateTime(
+                date.year,
+                date.month,
+                date.day,
+                beginDate.hour,
+                beginDate.minute,
+              );
+            }),
+            onBeginTimeChange: (DateTime time) => setState(() {
+              beginDate = DateTime(
+                beginDate.year,
+                beginDate.month,
+                beginDate.day,
+                time.hour,
+                time.minute,
+              );
+            }),
+            onEndDateChange: (DateTime date) => setState(() {
+              endDate = DateTime(
+                date.year,
+                date.month,
+                date.day,
+                endDate.hour,
+                endDate.minute,
+              );
+            }),
+            onEndTimeChange: (DateTime time) => setState(() {
+              endDate = DateTime(
+                endDate.year,
+                endDate.month,
+                endDate.day,
+                time.hour,
+                time.minute,
+              );
+            }),
+          ),
+          SizedBox(height: 40.0),
+          PrimaryButton(
+            title: 'Add',
+            onPressed: () {
+              if (!ProgressIntervalForm._key.currentState.validate()) return;
+              _intervals[title] = ProgressInterval(
+                begin: beginDate,
+                end: endDate,
+              );
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
